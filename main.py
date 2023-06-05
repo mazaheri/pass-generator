8e573a3ccf03b7f4a6eecb224bcbdd97732db034
Pourya Mazaheri <mazaheri.pourya@gmail.com>
2023-06-05 17:24:37 +0200

    first python code

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 16:29:52 2023
+
+@author: Pourya
+"""
+
+# password generator
+def hello():
+    return "hello world"
+    
+    
+
+if __name__ == "main.py":
+    hello()
+    
